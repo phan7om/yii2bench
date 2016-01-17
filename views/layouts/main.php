c1d6b431f0efc993dfbb272d2afbcfb5c12cc103
Captain Jack Sparrow <sd@freematiq.com>
2016-01-17 20:29:31 +0600

    Formated to normal answer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,15 +1,14 @@
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
+<html>
 <head>
     <meta charset="UTF-8" />
     <title>Welcome!</title>
 </head>
 <body>
-<?php $this->beginBody() ?>
-
-        <?= $content ?>
-
+<?php $this->beginBody() ?><?= $content ?>
 <?php $this->endBody() ?>
+
 </body>
 </html>
 <?php $this->endPage() ?>
